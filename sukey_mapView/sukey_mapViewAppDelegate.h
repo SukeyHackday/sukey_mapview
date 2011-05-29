22de0b7080aa795c3aa27de0b491ddcd9408eaf1
Tim Storey <tim.storey@gmail.com>
2011-05-29 14:43:57 +0100

    Initial Commit

---
+++ sukey_mapView/sukey_mapViewAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  sukey_mapViewAppDelegate.h
+//  sukey_mapView
+//
+//  Created by Tim Storey on 29/05/2011.
+//  Copyright 2011 Lateral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sukey_mapViewAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
